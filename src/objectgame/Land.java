a0aa6dbd54027714970e076dfc419a6ccc9fe9c0
1082878 <1082878@NCHS120490481.ms.nsd.org>
2024-05-23 12:24:47 -0700

    ground stuffs

--- src/objectgame/Land.java
+++ src/objectgame/Land.java
@@ -1,19 +1,23 @@
 package objectgame;
 
 import java.awt.Graphics;
+import static userinterface.GameScreen.GROUND;
 import java.awt.image.BufferedImage;
 
+import userinterface.GameScreen;
 import util.Sprite;
 
 public class Land {
 	
 	private BufferedImage imageLand;
 	
+	
 	public Land () {
 		imageLand = Sprite.getSpriteImage("data/background.png");
 	}
 	
 	public void draw(Graphics g) {
+		g.drawImage(imageLand, (int) GameScreen.GROUND,50, null);
 		
 	}
 }
